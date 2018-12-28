a7ee7ab2785792c9f076d5005e42cb7c62615fde
zhaoxiangfei <wingsnorth@hotmail.com>
2018-12-28 18:14:16 +0800

    add create account

--- read_block.py
+++ read_block.py
@@ -3,7 +3,7 @@ import struct,binascii,re
 
 indexpath = '/Users/zhaoxiangfei/code/testnet/testnet_node/blockchain/database/block_num_to_block/index'
 filepath = '/Users/zhaoxiangfei/code/testnet/testnet_node/blockchain/database/block_num_to_block/blocks'
-callfun={}
+
 
 class Unpackblock:
 
@@ -12,6 +12,7 @@ class Unpackblock:
         self.blockpath = block_path
         self.regop={}
         self.typefun={}
+        self.resultfun={}
         self.typefun['uint8']  = self.__unpack_uint8
         self.typefun['uint16'] = self.__unpack_uint16
         self.typefun['uint32'] = self.__unpack_uint32
@@ -21,6 +22,13 @@ class Unpackblock:
         self.typefun['bytes']  = self.__unpack_string
         self.typefun['asset_kinds'] =self.__unpack_asset_kinds
         self.typefun['memo']  = self.__unpack_memo_struct
+        self.typefun['sig'] = self.__unpack_sig
+        self.typefun['pk'] = self.__unpack_public_key
+        self.typefun['ripe160'] =self.__unpack_byte20
+        self.typefun['authority'] = self.__unpack_authority_struct
+        self.typefun['account_options'] = self.__unpack_account_options
+        self.resultfun['01']=self.__unpack_op_result_01
+        self.resultfun['03'] = self.__unpack_op_result_03
 
     def __unpack_uint_variant(self,stream):
         by ,v,index = 0 , 0, 0
@@ -51,6 +59,9 @@ class Unpackblock:
     def __unpack_uint64(self,stream):
         return stream[0:8],stream[8:]
 
+    def __unpack_byte20(self,stream):
+        return  stream[0:20],stream[20:]
+
     # string、vector、bytes
     def __unpack_string(self,stream):
         str_size,str_size_offset = self.__unpack_uint_variant(stream)
@@ -59,70 +70,107 @@ class Unpackblock:
     def __unpack_sig(self,stream):
         return stream[0:65],stream[65:]
 
+    def __unpack_public_key(self,stream):
+        return stream[0:33],stream[33:]
+
+    def __unpack_asset_kinds(self,stream):
+        msg, stream = self.__unpack_struct_array(stream,
+                                                 {'amount': 'uint64', 'id': 'uint'})
+        return msg,stream
+
+    def __unpack_memo_struct(self,stream):
+        msg,stream =self.__unpack_struct_array(stream,{'memo_from_key':'pk','memo_to_key':'pk','memo_nonce':'uint64','memo_msg(number+buffer':'string'})
+        return msg, stream
+
+    def __unpack_authority_struct(self,stream):
+        weight_threshold ,stream= self.__unpack_uint32(stream)
+        print('weight_threshold:',weight_threshold.hex())
+        account_auths,stream =self.__unpack_struct_array(stream,{'account_id_type':'uint','weight_type':'uint16'})
+        print('account_auths:',account_auths.hex())
+        key_auths, stream = self.__unpack_struct_array(stream, {'public_key_type': 'pk', 'weight_type': 'uint16'})
+        print('key_auths:', key_auths.hex())
+        address_auths, stream = self.__unpack_struct_array(stream, {'address': 'ripe160', 'weight_type': 'uint16'})
+        print('address_auths:', address_auths.hex())
+        return weight_threshold+account_auths+key_auths+account_auths,stream
+
+    def __unpack_account_options(self,stream):
+        memo_key,stream =self.__unpack_public_key(stream)
+        print('memo_key:',memo_key.hex())
+        voting_account,stream =self.__unpack_uint(stream)
+        print('voting_account:',voting_account.hex())
+        num_witness,stream =self.__unpack_uint16(stream)
+        print('num_witness:',num_witness.hex())
+        num_committee,stream =self.__unpack_uint16(stream)
+        print('num_committee:',num_committee.hex())
+        votes,stream =self.__unpack_struct_array(stream,{'vote_id':'uint32'})
+        print('votes:',votes.hex())
+        extensions,stream = self.__unpack_uint(stream)
+        print('extensions:',extensions.hex())
+        return memo_key+voting_account+num_witness+num_committee+votes+extensions,stream
+
+
+    def __unpack_struct_array(self,stream,type_mode):
+        num, offset = self.__unpack_uint_variant(stream)
+        str_msg, stream = self.__unpack_uint(stream)
+        msgs = str_msg
+        print('\n**** size:',str_msg.hex())
+        for i in range(0,num):
+            for key,value in type_mode.items():
+                msg,stream = self.typefun[value](stream)
+                print('****',key,':',msg.hex())
+                msgs = msgs +msg
+        return msgs,stream
+
+    def __unpack_op_result_01(self,stream):
+        msg,stream = self.__unpack_uint(stream)
+        print(msg.hex())
+        return msg,stream
+
+    def __unpack_op_result_03(self,stream):
+        billed_cpu_time_us, stream = self.__unpack_uint32(stream)
+        print('billed_cpu_time_us:', billed_cpu_time_us.hex())
+        ram_usage_bs, stream = self.__unpack_uint32(stream)
+        print('ram_usage_bs:', ram_usage_bs.hex())
+        fee_amount, stream = self.__unpack_uint64(stream)
+        print('fee_amount:', fee_amount.hex())
+        fee_id, stream = self.__unpack_uint(stream)
+        print('fee_id:', fee_id.hex())
+        return billed_cpu_time_us+ram_usage_bs+fee_amount+fee_id,stream
+
     def __unpack_op_result(self,stream):
         number, str_offset = self.__unpack_uint_variant(stream)
         str_res,stream = self.__unpack_uint(stream)
         print('op_res_number:',str_res.hex())
         for i in range(0,number):
             op_res_code,stream = self.__unpack_uint(stream)
+            print('op_res_code:',op_res_code.hex())
             if op_res_code[0] == 0:
                 break
-            print('op_res_code:',op_res_code.hex())
-            billed_cpu_time_us,stream = self.__unpack_uint32(stream)
-            print('billed_cpu_time_us:',billed_cpu_time_us.hex())
-            ram_usage_bs,stream = self.__unpack_uint32(stream)
-            print('ram_usage_bs:',ram_usage_bs.hex())
-            fee_amount, stream = self.__unpack_uint64(stream)
-            print('fee_amount:', fee_amount.hex())
-            fee_id, stream = self.__unpack_uint(stream)
-            print('fee_id:', fee_id.hex())
+            else:
+                self.resultfun[op_res_code.hex()](stream)
         return stream
 
-    def __unpack_asset_kinds(self,stream):
-        str_num, str_size_offset = self.__unpack_uint_variant(stream)
-        str_asset,stream = self.__unpack_uint(stream)
-        asset_kinds = str_asset          #资产种类数目
-        for i in range(0,str_num):
-            amount,stream = self.__unpack_uint64(stream)
-            id,stream = self.__unpack_uint(stream)
-            asset_kinds =asset_kinds +amount +id
-        return asset_kinds,stream
+    def __unpackopdata(self,stream,op_code):
+        if op_code in self.regop:
+            for pair in self.regop[op_code]:
+                (key, value), = pair.items()
+                if key in self.typefun:
+                    curr_buff,stream = self.typefun[key](stream)
+                    print(value,':',curr_buff.hex())
+                else :
+                    print('not found ',key,' type')
+                    break
+            return stream
+        else :
+            print('op not found , please reg op')
 
-    def __unpack_sig_struct(self,stream):
-        str_num, str_size_offset = self.__unpack_uint_variant(stream)
-        str_sig,stream = self.__unpack_uint(stream)
-        signatures = str_sig            # number+sig1+sig2....
-        for i in range(0,str_num):
-            sig,stream = self.__unpack_sig(stream)
-            signatures =signatures +sig
-        return signatures,stream
-
-    def __unpack_public_key(self,stream):
-        return stream[0:33],stream[33:]
-
-    def __unpack_memo_struct(self,stream):
-        str_num, str_size_offset = self.__unpack_uint_variant(stream)
-        str_memo, stream = self.__unpack_uint(stream)
-        print('\n----memo_number:', str_memo.hex())
-        memos = str_memo
-        for i in range(0, str_num):
-            pk_from, stream = self.__unpack_public_key(stream)
-            print('----memo_from_key:',pk_from.hex())
-
-            pk_to, stream = self.__unpack_public_key(stream)
-            print('----memo_to_key:', pk_to.hex())
-
-            nonce, stream = self.__unpack_uint64(stream)
-            print('----memo_nonce:', nonce.hex())
-
-            msg, stream = self.__unpack_string(stream)
-            print('----memo_msg(number+buffer):', msg.hex())
-
-            memos = memos + pk_from + pk_to + nonce +msg
-        return memos, stream
-
-    def reg_op_struct(self,op_code,op_struct):
-        self.regop[op_code] = op_struct
+    def __unpackenderdata(self,stream):
+        ext,stream = self.__unpack_uint8(stream)
+        print('tr_extensions:',ext.hex())  #由于该字段多数区块均为0，所以未处理复杂情况，仅当做单字节处理
+        signatures, stream = self.__unpack_struct_array(stream,{'signtures':'sig'})
+        print('signatures(number+sigs):',signatures.hex())
+        stream = self.__unpack_op_result(stream)
+        return stream
 
     def unpackblockbynum(self,blocknumber):
         offset = blocknumber * 0x20
@@ -141,41 +189,22 @@ class Unpackblock:
             print('id:', id.hex(), "\n\n")
             self.__unpackblockdata(offs, size)
 
-    def __unpackopdata(self,stream,op_code):
-        if op_code in self.regop:
-            for pair in self.regop[op_code]:
-                (key, value), = pair.items()
-                if key in self.typefun:
-                    curr_buff,stream = self.typefun[key](stream)
-                    print(value,':',curr_buff.hex())
-                else :
-                    print('not found ',key,' type')
-                    break
-            return stream
-        else :
-            print('op not found , please reg op')
-    def __unpackenderdata(self,stream):
-        ext,stream = self.__unpack_uint8(stream)
-        print('tr_extensions:',ext.hex())  #由于该字段多数区块均为0，所以未处理复杂情况，仅当做单字节处理
-        signatures,stream = self.__unpack_sig_struct(stream)
-        print('signatures(number+sigs):',signatures.hex())
-        stream = self.__unpack_op_result(stream)
-        return stream
+    def reg_op_struct(self,op_code,op_struct):
+        self.regop[op_code] = op_struct
 
     def __unpackblockdata(self,offset,size):
         with open(filepath, 'rb') as file:
             file.seek(offset, 0)
             lines = file.read(size)
             hex_lines = binascii.b2a_hex(lines)
-            # result = re.sub(r"(?<=\w)(?=(?:\w\w)+$)", " ", str(hex_lines)[2:-1])
-            print("block raw data:", hex_lines[0:-1])
+            print("\n-------------block header-----------\n")
+            print("block raw data:", hex_lines)
 
             # block_header
             header = lines[0:46]
             fmt_block_header = '20sIB20sB'
             previous, timestamp, witness, transaction_merkle_root, extensions = struct.unpack(fmt_block_header, header)
-            print("\n-------------block header-----------\n")
-            print('previous:', previous.hex())
+            print('\nprevious:', previous.hex())
             print('timestamp:', hex(timestamp)[2:].zfill(8))
             print('witness:', hex(witness)[2:].zfill(2))
             print('transaction_merkle_root:', transaction_merkle_root.hex())
@@ -237,13 +266,15 @@ if __name__ == '__main__':
 
     # 解析部署合约操作
     # todo
-    
+
     # 解析转账操作
-    transfer_dic = ({"uint8":'op_code'},{"uint64":"fee_amount"},{"uint" :'fee_id'},{"uint" :'from'},{'uint':'to'},{"uint64":"amount_amount"},{"uint" :'amount_id'},{"memo":"memo_num"},{'uint8':'op_extensions'})
+    transfer_dic = ({"uint8":'op_code'},{"uint64":"fee_amount"},{"uint" :'fee_id'},{"uint" :'from'},{'uint':'to'},{"uint64":"amount_amount"},{"uint" :'amount_id'},{"memo":"memo"},{'uint8':'op_extensions'})
     obj.reg_op_struct(0,transfer_dic)
 
-    # ........
+    # 解析创建账户操作
+    createaccount_dic =({"uint8":'op_code'},{"uint64":"fee_amount"},{"uint" :'fee_id'},{"uint" :'registrar'},{'uint':'referrer'},{'uint16':'referrer_percent'},{'string':'name'},{'authority':'owner'},{'authority':'active'},{'account_options':'options'},{'uint':'op_extensions'})
+    obj.reg_op_struct(5,createaccount_dic)
 
     # unpack指定区块
-    #obj.unpackblockbynum(10012266)
-    obj.unpackblockbynum(9912162)
+    #obj.unpackblockbynum(9841632)
+    obj.unpackblockbynum(9841632)
